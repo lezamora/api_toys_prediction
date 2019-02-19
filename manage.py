d8110532f81b5e662a9c0fd9df082edfba4b8129
Zamora <Lzamora@agea.com.ar>
2019-02-19 16:32:03 -0300

    Fix

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_script import Manager
 from app import blueprint
 from app.main import create_app
 
-app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+app = create_app(os.getenv('DEPLOY_ENV') or 'dev')
 app.register_blueprint(blueprint)
 
 app.app_context().push()
