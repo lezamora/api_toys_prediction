59ecf86aefdb99c243b1f8fdd5fd05e0fd8c1309
lezamora <lucas.e.zamora@gmail.com>
2019-06-14 15:52:17 -0300

    fix parcial

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from werkzeug.contrib.fixers import ProxyFix
 
 from .config import config_by_name
 
@@ -13,6 +14,7 @@ flask_bcrypt = Bcrypt()
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.config.from_object(config_by_name[config_name])
     flask_bcrypt.init_app(app)
 
